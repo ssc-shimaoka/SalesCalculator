a7b3b5cb6f07973a5b3d3381bf724d976b877c08
yuki shimaoka_home <yuki_shimaoka@solver.co.jp>
2020-09-08 22:00:15 +0900

    ・実装完了

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -3,13 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SalesCalculator
 {
     class Program
     {
+        // ************************************************************
+        // main
+        // ************************************************************
         static void Main(string[] args)
         {
+            // SalesCounterインスタンス生成・ファイルから読み込み・リスト生成
+            SalesCounter sales = new SalesCounter(ReadSales("sales.csv"));
+
+            // 売り上げ金額取得
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+
+            // 画面出力
+            foreach(KeyValuePair<string, int> obj in amountPerStore)
+            {
+                Console.WriteLine("{0}{1}", obj.Key, obj.Value);
+            }
+
+        }
+
+        // ************************************************************
+        // 売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        // ************************************************************
+        static List<Sale> ReadSales(string filePath)
+        {
+            // リスト生成
+            List<Sale> sales = new List<Sale>();
+
+            // ファイル全読み込み
+            string[] lines = File.ReadAllLines(filePath);
+
+            // 解析
+            foreach(string line in lines)
+            {
+                // 項目に分ける
+                string[] iteams = line.Split(',');
+
+                // リスト生成・初期化
+                Sale sale = new Sale {
+                    ShopName        = iteams[0],
+                    ProductCategory = iteams[1],
+                    Amount          = int.Parse(iteams[2])
+                };
+
+                // リストに追加
+                sales.Add(sale);
+            }
+
+            return sales;
         }
     }
 }
---
+++ SalesCalculator/SalesCounter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator
+{
+    public class SalesCounter
+    {
+        // 計算用リスト
+        private List<Sale> _sales;
+
+        //**************************************************************
+        // コンストラクタ
+        //**************************************************************
+        public SalesCounter(List<Sale> sales)
+        {
+            _sales = sales;
+        }
+
+        //**************************************************************
+        // 店舗別売上処理
+        //**************************************************************
+        public Dictionary<string, int> GetPerStoreSales()
+        {
+            // Dictionaryクラスのインスタンス生成
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            foreach(Sale sale in _sales)
+            {
+                if(dict.ContainsKey(sale.ShopName))
+                {
+                    dict[sale.ShopName] += sale.Amount;
+                }
+                else
+                {
+                    dict[sale.ShopName] = sale.Amount;
+                }
+            }
+
+            return dict;
+
+
+        }
+    }
+}
